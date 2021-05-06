04573f3743471c09337ee817d360c1fbd08296fa
Michał Lepszy <lepszy97@yahoo.com>
2021-05-06 21:16:47 +0200

    FirstClass & HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Michał");
+        System.out.println(42);
+        System.out.println('L');
+    }
+}
