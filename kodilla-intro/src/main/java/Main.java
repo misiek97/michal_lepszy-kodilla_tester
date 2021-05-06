49f015fd3cbca3e0da955fca014a10047e2920f4
Michał Lepszy <lepszy97@yahoo.com>
2021-05-06 21:57:36 +0200

    zmienne

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String myText = "Hello from FirstClass!";
+        int myAge = 42;
+        double height = 1.65;
+        char carNo = '0';
+        boolean isGoodProgrammer = true;
+
+        System.out.println(myText);
+    }
+    }
