8a60abc929649956cf50ce7df62310fa94c6d42b
Michał Lepszy <lepszy97@yahoo.com>
2021-05-06 22:11:39 +0200

    zmienne cd.

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myTextStart = "Tomorrow's wheather";
+        String  myTextEnd= "Thank you for attention";
+        int temp = 10;
+        double rainDrop = 5.50;
+        char visib = '8';
+        boolean isWheatherRainny = true;
+
+        System.out.println(myTextStart);
+        System.out.println(temp);
+        System.out.println(rainDrop);
+        System.out.println(visib);
+        System.out.println(isWheatherRainny);
+        System.out.println(myTextEnd);
+    }
+    }
