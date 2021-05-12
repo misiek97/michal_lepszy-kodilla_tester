55b378ace64d8b987f92bdeaf58514a814bc1b93
Michał Lepszy <lepszy97@yahoo.com>
2021-05-12 13:18:20 +0200

    loop

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+        int numberOfNames = names.length;
+
+        for (int i = 0; i < numberOfNames; i++) {
+            String name = names[i];
+            System.out.println(name);
+        }
+
+        int[] digits = new int[] {21, 2, 13, 14, 95, 6};
+        int numberOfDigits = digits.length;
+        int sum = digits [1]
+
+        for (int i = numberOfDigits - 1; i >= 0; i--) {
+
+
+        }
+    }
+}
