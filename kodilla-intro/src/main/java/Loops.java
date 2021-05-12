f8ac789e454851a07c5f30965f852ef2abd4e4ff
Michał Lepszy <lepszy97@yahoo.com>
2021-05-12 22:04:05 +0200

    verification with tutor

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -15,7 +15,6 @@ public class Loops {
     public static int sum(int[] numbers) {
 
         int sum = 0;
-
         for (int i = numbers.length - 1; i >= 0; i--) {
             sum = sum + numbers[i];
         }
