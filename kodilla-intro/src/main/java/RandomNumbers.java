ee1318bd6d75570ae375b3ea0861ee87582aba70
Michał Lepszy <lepszy97@yahoo.com>
2021-05-30 16:42:29 +0200

    2.5 Random

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -1,15 +1,25 @@
 import java.util.Random;
 class RandomNumbers {
     public static void main(String args[]) {
-        Random rand = new Random();   //instance of random class
-        int maxValue = 31;        //generate random values from 0-30
-        int int_random = rand.nextInt(maxValue);
+        Random rand = new Random(); //instance of random class
+        int maxValue = 31; //generate random values from 0-30
+        int number = rand.nextInt(maxValue);
 
-        int sum = int_random++;
-        if (sum < 5000) {
+        int min = 0;
+        int max = 0;
 
+        for (int sum = number; sum < 5000; sum += number) {
+            number = rand.nextInt(maxValue);
 
+            if (min > number) {
+                min = number;
+            }
+
+            if (max < number) {
+                max = number;
+            }
         }
-
+        System.out.println(min);
+        System.out.println(max);
     }
 }
