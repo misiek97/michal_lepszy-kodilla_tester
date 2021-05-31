1cbbbfce9c705927a8b41c557908cedefddc6ec6
Michał Lepszy <lepszy97@yahoo.com>
2021-05-31 23:02:23 +0200

    2.6 Refaktoryzacja

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,28 +1,6 @@
 public class Application {
-    public static void main(String[] args) {
-        Application user = new Application("Adam", 40.5, 178);
+    public void main(String[] args) {
+        ApplicationUser user = new ApplicationUser ("Adam", 40.5, 178);
         user.checkData();
-    }
-
-    public class User {
-        String name;
-        double age;
-        double heigh;
-
-        public User(String name, double age, double heigh;) {
-            this.name = name;
-            this.age = age;
-            this.heigh = heigh;
-        }
-
-        public void checkData() {
-            if (name != null) {
-                if (this.age > 30 && this.height > 160) {
-                    System.out.println("User is older than 30 and higher than 160cm");
-                } else {
-                    System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-                }
-            }
-        }
     }
 }
---
+++ kodilla-intro/src/main/java/ApplicationUser.java
@@ -0,0 +1,21 @@
+public class ApplicationUser {
+    String name;
+    double age;
+    double height;
+
+    public ApplicationUser(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void checkData() {
+        if (name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+            }
+        }
+    }
+}
