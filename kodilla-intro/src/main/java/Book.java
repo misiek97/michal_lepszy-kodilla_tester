e7a2359d25bfd9fd49fddc5c2c44ac86fb7354dd
Michał Lepszy <lepszy97@yahoo.com>
2021-06-06 09:18:38 +0200

    2.7 Zmienne i metody statyczne

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -2,14 +2,16 @@ public class Book {
     private String author;
     private String title;
 
-    Book book = Book.of("Issac Asimov", "Galaxy");
+    public static Book of (String author, String title) {
+        return new Book (author, title);
+    }
 
-    public static String of (String author, String title) {
-        return author;
-        return title;
+    public Book (String author, String title) {
+        Book book = Book.of("Isaac Assimov", "Galaxy");
+        }
 
+    public static void main(String[] args) {
+        Book book = Book.of("Isaac Assimov", "Galaxy");
+        System.out.println(book);
     }
 }
-
-
-
